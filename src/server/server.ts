9c2385b8e3906e06af523e3ae75c8120ecfa89e0
alexandreev1 <alexandreev0210@gmail.com>
2019-09-15 19:43:37 +0300

    Бета-версия. Рабочий сервак, ORM

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,13 +1,28 @@
-// @ts-ignore
 import express from "express";
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import Book from "../entity/Book";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+createConnection()
+    .then(
+        async (connection) => {
+            const bookRepository = connection.getRepository(Book);
+            const books = await bookRepository.find({ relations: ["persons"], take: 1000 });
 
-app.listen(3000, () => {
-    // tslint:disable-next-line:no-console
-    console.log(`##### app on the port ${ port } now #####`);
-});
+            app.get("/api/books", (req, res) => {
+                res.send(JSON.stringify(books));
+            });
+
+            app.listen(3000, () => {
+                // tslint:disable-next-line:no-console
+                console.log(`##### app on the port ${ port } now #####`);
+            });
+
+        })
+    .catch((error) => {
+            // tslint:disable-next-line:no-console
+            console.log(error);
+        }
+    );
